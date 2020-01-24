46dc1b1f335b956cbbe3e6a6e90265f20b45cdf7
patrickkeough <55281138+patrickkeough@users.noreply.github.com>
2020-01-24 08:26:16 -0500

    Create myScripts.js

---
+++ myScripts.js
@@ -0,0 +1,18 @@
+function myLoop(){
+  var start = document.getElementById("start").value;
+  var end = document.getElementById("end").value;
+  var jump = document.getElementById("count").value;
+  
+  console.log(typeof start);
+  console.log(typeof end);
+  console.log(typeof jump);
+  
+  start = Number(start)
+  end = Number(end)
+  jump = Number(jump)
+  
+for( var i = start; i <= end; i+=jump)
+{
+  document.write('<p>' + i + '</p>')
+}
+}
